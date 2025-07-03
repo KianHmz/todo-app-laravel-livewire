4b0fc58d7567052c6652d77b76a6c20a5dc99083
KianHmz <hmz00abd@gmail.com>
2025-07-04 00:07:55 +0330

    add folders & tasks model

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function task()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
