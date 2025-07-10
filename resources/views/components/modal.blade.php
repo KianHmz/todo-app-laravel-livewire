b66c4a42ab0143bf9e93a7610772969b581cd26d
KianHmz <hmz00abd@gmail.com>
2025-07-10 15:18:22 +0330

    refactor routes

--- resources/views/components/modal.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['id'])
-
-<div id="{{ $id }}" class="fixed inset-0 bg-black/30 hidden justify-center items-center z-50">
-    <div class="bg-white dark:bg-gray-800 p-8 rounded-lg w-1/3 relative">
-        <button class="absolute top-2 right-2 text-gray-400 closeModalBtn">&times;</button>
-        {{ $slot }}
-    </div>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,6 @@ Route::get('/password/request', function () {
 })->name('password.request');
 
 
-// Route::get('/dashboard/{folder}', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::get('/dashboard', function(){return view('dashboard');})->name('dashboard.index');
-
-Route::post('/folders/store', [FolderController::class, 'store'])->name('folders.store');
-Route::put('/folders/{folder}/update', [FolderController::class, 'update'])->name('folders.update');
-Route::delete('/folders/{folder}/destroy', [FolderController::class, 'destroy'])->name('folders.destroy');
-
-Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
-Route::put('/tasks/{task}/update', [TaskController::class, 'update'])->name('tasks.update');
-Route::put('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
-Route::delete('/tasks/{task}/destroy', [TaskController::class, 'destroy'])->name('tasks.destroy');
-
-Route::get('/',function(){
-    return view('welcome');
-});
+Route::get('/', function () {
+    return view('dashboard'); })->name('dashboard.index');
 
